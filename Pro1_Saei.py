49291216751f9fa4e8b088a9a0fdd0d0e9b5548c
Alireza Saei <saeialireza359@gmail.com>
2022-05-22 20:10:10 +0430

    Gets input and prints grammar

--- Pro1_Saei.py
+++ Pro1_Saei.py
@@ -1,7 +1,29 @@
+from collections import defaultdict
+
+
+def printGrammar():
+    for key in G.keys():
+        for item in G[key]:
+            print(key, " -> ", item)
+
+
+G = defaultdict()
 while (True):
     try:
         # get lines of # sepreated right linear grammar
         grammar = input()
-
-    except (EOFError):
+        if(not grammar[-1].isupper()):
+            raise ValueError("Grammar Is Not Right Linear")
+        if not grammar[0] in G:
+            temp = list()
+            temp.append(grammar[2:])
+            G[grammar[0]] = temp
+        else:
+            li = G[grammar[0]]
+            li.append(grammar[2:])
+            G[grammar[0]] = li
+    except (Exception) as e:
+        print(e)
         break
+
+printGrammar()

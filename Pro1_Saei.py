ba48e9b0636e5b5e670c53809e9132e7a89142ac
Alireza Saei <saeialireza359@gmail.com>
2022-05-23 21:48:13 +0430

    Deletes Useless Transitions

--- Pro1_Saei.py
+++ Pro1_Saei.py
@@ -2,9 +2,27 @@ from collections import defaultdict
 
 
 def printGrammar():
+    print("Right Linear Grammar: ")
     for key in G.keys():
         for item in G[key]:
             print(key, " -> ", item)
+
+
+def trim():
+    x = G.keys()
+    for key in x:
+        temp = []
+        for item in G[key]:
+            if not (item[-1] in x) and (item[-1].isupper()):
+                print("Deleted", key, item)
+                temp.append(item)
+        li = G[key]
+
+        for i in temp:
+            if i in li:
+                li.remove(i)
+
+        G[key] = li
 
 
 G = defaultdict()
@@ -12,8 +30,6 @@ while (True):
     try:
         # get lines of # sepreated right linear grammar
         grammar = input()
-        if(not grammar[-1].isupper()):
-            raise ValueError("Grammar Is Not Right Linear")
         if not grammar[0] in G:
             temp = list()
             temp.append(grammar[2:])
@@ -22,8 +38,8 @@ while (True):
             li = G[grammar[0]]
             li.append(grammar[2:])
             G[grammar[0]] = li
-    except (Exception) as e:
-        print(e)
+    except Exception:
         break
 
+trim()
 printGrammar()

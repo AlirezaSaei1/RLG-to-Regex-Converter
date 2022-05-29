d9aa886fd43a7d7b6e7248d65eb3303a72289a66
Alireza Saei <saeialireza359@gmail.com>
2022-05-29 14:36:32 +0430

    build NFA from Grammar

--- Pro1_Saei.py
+++ Pro1_Saei.py
@@ -1,6 +1,41 @@
 from collections import defaultdict
 
 
+class FA():
+    states = []
+
+    def __init__(self) -> None:
+        for key in G.keys():
+            state = State(key)
+            self.states.append(state)
+
+            if "Start" in key:
+                self.startState = state
+
+        self.finalState = State("Final")
+        self.states.append(self.finalState)
+
+    # def p(self):
+    #     for x in self.states:
+    #         print(x.stateName)
+    #         for i in x.transitions.keys():
+    #             print("Ts:")
+    #             print(i, x.transitions[i])
+
+
+class State():
+    def __init__(self, name):
+        self.stateName = name
+        self.transitions = defaultdict()
+
+        if name != "Final":
+            for x in G[name]:
+                if x[-1].isupper():
+                    self.transitions[x[:-1]] = x[-1]
+                else:
+                    self.transitions[x] = "Final"
+
+
 def printGrammar():
     print("Right Linear Grammar: ")
     for key in G.keys():
@@ -14,7 +49,6 @@ def trim():
         temp = []
         for item in G[key]:
             if not (item[-1] in x) and (item[-1].isupper()):
-                print("Deleted", key, item)
                 temp.append(item)
         li = G[key]
 
@@ -25,21 +59,31 @@ def trim():
         G[key] = li
 
 
-G = defaultdict()
-while (True):
-    try:
-        # get lines of # sepreated right linear grammar
-        grammar = input()
-        if not grammar[0] in G:
-            temp = list()
-            temp.append(grammar[2:])
-            G[grammar[0]] = temp
-        else:
-            li = G[grammar[0]]
-            li.append(grammar[2:])
-            G[grammar[0]] = li
-    except Exception:
-        break
+if __name__ == "__main__":
+    G = defaultdict()
+    adj_matrix = defaultdict(dict)
 
-trim()
-printGrammar()
+    li1 = []
+    li1.append("λS")
+    G["Start"] = li1
+
+    while (True):
+        try:
+            # get lines of # sepreated right linear grammar
+            grammar = input()
+            if not grammar[0] in G:
+                temp = list()
+                temp.append(grammar[2:])
+                G[grammar[0]] = temp
+            else:
+                li = G[grammar[0]]
+                li.append(grammar[2:])
+                G[grammar[0]] = li
+        except Exception:
+            break
+
+    trim()
+    printGrammar()
+
+    fa = FA()
+    # fa.p()
